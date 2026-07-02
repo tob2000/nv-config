5be9d3465762da4f5e881843527d967762f97b8e
Tobias Astrom <tobias@coresolution.net>
2026-07-02 21:10:06 +0200

    larger preview git status / snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,25 +1,48 @@
- return {
+return {
   {
-  "folke/snacks.nvim",
-  opts = {
-  picker = {
-  win = {
-  input = {
-  keys = {
-         -- tex Git Status
-  ["<PageDown>"] = { "preview_scroll_down", mode = { "i", "n" } },
-  ["<PageUp>"] = { "preview_scroll_up", mode = { "i", "n" } },
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        win = {
+          input = {
+            keys = {
+              -- tex Git Status
+              ["<PageDown>"] = { "preview_scroll_down", mode = { "i", "n" } },
+              ["<PageUp>"] = { "preview_scroll_up", mode = { "i", "n" } },
+            },
+          },
+          list = {
+            keys = {
+              -- tex Git Status
+              ["<PageDown>"] = "preview_scroll_down",
+              ["<PageUp>"] = "preview_scroll_up",
+            },
+          },
+        },
+
+        -- Anpassa layout för git_status: höger panel (preview) = 70% av bredden
+        -- (vänster = fil-listan, höger = diff-preview)
+        sources = {
+          git_status = {
+            layout = {
+              layout = {
+                box = "horizontal",
+                width = 0.8,
+                min_width = 120,
+                height = 0.8,
+                {
+                  box = "vertical",
+                  border = true,
+                  title = "{title} {live} {flags}",
+                  { win = "input", height = 1, border = "bottom" },
+                  { win = "list", border = "none" },
+                },
+                { win = "preview", title = "{preview}", border = true, width = 0.70 },
+              },
+            },
+          },
+        },
+      },
+    },
   },
-  },
-  list = {
-  keys = {
-         -- tex Git Status
-  ["<PageDown>"] = "preview_scroll_down",
-  ["<PageUp>"] = "preview_scroll_up",
-  },
-  },
-  },
-  },
-  },
-  },
- }
+}
